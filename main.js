59ee0ac55949f025f4dfb2c97b268c25f693f5ae
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-09-29 08:46:04 -0500

    Ejercicio #4

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 
-
 let contador = 0;
-    alert("Ahora calcularemos la nota promedio de 5 estudiantes");
-while (contador < 5){
+    alert("Ahora calcularemos cuantas damas había en el baile");
+while (contador < 1){
     contador ++;
-    let nombre = prompt("Ingrese su nombre ");
-    let apellido = prompt("Ingrese su apellido: " + nombre);
-    let edad = Number(prompt("Ingrese el edad : " + nombre));
-    let nota1 = Number(prompt("Ingrese su nota #1 : " + nombre));
-    let nota2 = Number(prompt("Ingrese su nota #2 : " + nombre));
-    let nota3 = Number(prompt("Ingrese su nota #3 : " + nombre));
-    let promedio = nota1 + nota2 + nota3 / 3;
-            alert("La nota promedio de: " + nombre + " es de: "  + promedio);
-    
+    alert("Hay 42 personas" + "\n" +
+            "Una dama bailó con 6 hombres");
+let personas = 42;
+let primdama = personas - 6
+        alert("Quedan: " + primdama + " personas y hay una dama de momento");
+let totaldamas = primdama / 2;
+        alert("Siguiendo la lógica del problema, hay: " + totaldamas + " damas" + "\n" +
+               "y 24 caballeros");
 }
