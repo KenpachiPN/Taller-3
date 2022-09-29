eb0d95061d6a11ec1aa5f48474c137991642223d
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-09-29 08:34:07 -0500

    Ejercicio #3

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 
 
 let contador = 0;
-while (contador < 10){
+    alert("Ahora calcularemos la nota promedio de 5 estudiantes");
+while (contador < 5){
     contador ++;
-        alert("Ahora haremos operaciones matemáticas básicas");
-    let num1 = Number(prompt("Ingrese el número: " + contador));
-    let num2 = Number(prompt("Ingrese el número: " + contador));
-    let suma = num1 + num2;
-        alert("La suma de: " + num1 + " y " + num2 + " es de: " + suma);
-    let resta = num1 - num2;
-        alert("La resta de: " + num1 + " y " + num2 + " es de: " + resta);
-    let multi = num1 * num2;
-        alert("La multiplicación de: " + num1 + " y " + num2 + " es de: " + multi);
-    let divis = num1 - num2;
-        alert("La división de: " + num1 + " y " + num2 + " es de: " + divis);
+    let nombre = prompt("Ingrese su nombre ");
+    let apellido = prompt("Ingrese su apellido: " + nombre);
+    let edad = Number(prompt("Ingrese el edad : " + nombre));
+    let nota1 = Number(prompt("Ingrese su nota #1 : " + nombre));
+    let nota2 = Number(prompt("Ingrese su nota #2 : " + nombre));
+    let nota3 = Number(prompt("Ingrese su nota #3 : " + nombre));
+    let promedio = nota1 + nota2 + nota3 / 3;
+            alert("La nota promedio de: " + nombre + " es de: "  + promedio);
+    
 }
