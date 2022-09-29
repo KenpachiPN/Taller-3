b947e5fe2ddbfd985ff2e5a47f85d1b0cb69439f
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-09-29 08:56:35 -0500

    Ejercicio #5

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
 let contador = 0;
-    alert("Ahora calcularemos cuantas damas había en el baile");
-while (contador < 1){
+    alert("Ahora calcularemos cuantos estudiantes no tienen derecho al exámen de nivelación");
+while (contador < 5){
     contador ++;
-    alert("Hay 42 personas" + "\n" +
-            "Una dama bailó con 6 hombres");
-let personas = 42;
-let primdama = personas - 6
-        alert("Quedan: " + primdama + " personas y hay una dama de momento");
-let totaldamas = primdama / 2;
-        alert("Siguiendo la lógica del problema, hay: " + totaldamas + " damas" + "\n" +
-               "y 24 caballeros");
+    let nombre = prompt("Ingrese su nombre ");
+    let nota1 = Number(prompt("Ingrese su nota #1 : " + nombre));
+    let nota2 = Number(prompt("Ingrese su nota #2 : " + nombre));
+    let nota3 = Number(prompt("Ingrese su nota #3 : " + nombre));
+    let nota4 = Number(prompt("Ingrese su nota #4 : " + nombre));
+    let nota5 = Number(prompt("Ingrese su nota #5 : " + nombre));
+    let promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+        alert("La nota promedio de " + nombre + " es de: "  + promedio);
+    if( promedio >= 3.0) {
+        alert(nombre + " tiene derecho a presentar el examen de nivelación :'D");
+    }
+    else{
+        alert(nombre + " no tiene derecho a presentar el examen de nivelación :'(");
+    }
+    
 }
