06e18416c954918769fb594aca67b23c7e7932b6
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-09-29 09:12:35 -0500

    Ejercicio #6

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 
-let contador = 0;
-    alert("Ahora calcularemos cuantos estudiantes no tienen derecho al exámen de nivelación");
-while (contador < 5){
-    contador ++;
-    let nombre = prompt("Ingrese su nombre ");
-    let nota1 = Number(prompt("Ingrese su nota #1 : " + nombre));
-    let nota2 = Number(prompt("Ingrese su nota #2 : " + nombre));
-    let nota3 = Number(prompt("Ingrese su nota #3 : " + nombre));
-    let nota4 = Number(prompt("Ingrese su nota #4 : " + nombre));
-    let nota5 = Number(prompt("Ingrese su nota #5 : " + nombre));
-    let promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
-        alert("La nota promedio de " + nombre + " es de: "  + promedio);
-    if( promedio >= 3.0) {
-        alert(nombre + " tiene derecho a presentar el examen de nivelación :'D");
-    }
-    else{
-        alert(nombre + " no tiene derecho a presentar el examen de nivelación :'(");
-    }
-    
+    alert("Ahora calcularemos la distancia que separa al avión del proyectil, hasta que estén a 10,000 mtrs. o más");
+    let segundos = 0;
+    let avion = 222222;
+    let misil = 222222;
+    let acel = 20;
+    let acem = 10;
+while (acel - acem <= 10000){
+    segundos ++;
+    acel += 20 + acel
+    acem += 10 + acem
+        alert("En " + segundos + " Segundos");
+        alert("La distancia del avion es: " + acel);
+        alert("La distancia del misil es: " + acem);
 }
